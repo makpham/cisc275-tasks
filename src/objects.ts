b2d89de49a5600149ded1023777a37993dedeb5f
mak <makpham@udel.edu>
2024-02-28 13:53:16 -0500

    Fixed parsing. Missing } at end

--- src/objects.ts
+++ src/objects.ts
@@ -80,75 +80,87 @@ export function toShortForm(question: Question): string {
  */
 export function toMarkdown(question: Question): string {
     let result = `# ${question.name}\n`;
-    result += `${question.body}\n`
+    result += `${question.body}\n`;
 
     if (question.type === "multiple_choice_question") {
         const options = question.options;
         for (let i = 0; i < options.length; i++) {
-            result += `- ${options[i]}\n;`
+            result += `- ${options[i]}\n;`;
+        }
+        return result;
     }
-    return result
-}
 
-/**
- * Return a new version of the given question, except the name should now be
- * `newName`.
- */
-export function renameQuestion(question: Question, newName: string): Question {
-    return {...question, name: newName,};
-}
+    /**
+     * Return a new version of the given question, except the name should now be
+     * `newName`.
+     */
+    export function renameQuestion(
+        question: Question,
+        newName: string
+    ): Question {
+        return { ...question, name: newName };
+    }
 
-/**
- * Return a new version of the given question, except the `published` field
- * should be inverted. If the question was not published, now it should be
- * published; if it was published, now it should be not published.
- */
-export function publishQuestion(question: Question): Question {
-    return { ...question, published: question.published ? false : true };
-}
+    /**
+     * Return a new version of the given question, except the `published` field
+     * should be inverted. If the question was not published, now it should be
+     * published; if it was published, now it should be not published.
+     */
+    export function publishQuestion(question: Question): Question {
+        return { ...question, published: question.published ? false : true };
+    }
 
-/**
- * Create a new question based on the old question, copying over its `body`, `type`,
- * `options`, `expected`, and `points` without changes. The `name` should be copied
- * over as "Copy of ORIGINAL NAME" (e.g., so "Question 1" would become "Copy of Question 1").
- * The `published` field should be reset to false.
- */
-export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    const newQuestion: Question = {
-        id, name: `Copy of ${oldQuestion.name}`, body: oldQuestion.body, type: oldQuestion.type, options: oldQuestion.options, expected: oldQuestion.expected, points: oldQuestion.points, published: false,
-        SHORT_ANSWER: "",
-        MULTIPLE_CHOCE: ""
-    };
+    /**
+     * Create a new question based on the old question, copying over its `body`, `type`,
+     * `options`, `expected`, and `points` without changes. The `name` should be copied
+     * over as "Copy of ORIGINAL NAME" (e.g., so "Question 1" would become "Copy of Question 1").
+     * The `published` field should be reset to false.
+     */
+    export function duplicateQuestion(
+        id: number,
+        oldQuestion: Question
+    ): Question {
+        const newQuestion: Question = {
+            id,
+            name: `Copy of ${oldQuestion.name}`,
+            body: oldQuestion.body,
+            type: oldQuestion.type,
+            options: oldQuestion.options,
+            expected: oldQuestion.expected,
+            points: oldQuestion.points,
+            published: false,
+            SHORT_ANSWER: "",
+            MULTIPLE_CHOCE: ""
+        };
 
-    return newQuestion;
-}
+        return newQuestion;
+    }
 
-/**
- * Return a new version of the given question, with the `newOption` added to
- * the list of existing `options`. Remember that the new Question MUST have
- * its own separate copy of the `options` list, rather than the same reference
- * to the original question's list!
- * Check out the subsection about "Nested Fields" for more information.
- */
-export function addOption(question: Question, newOption: string): Question {
-    return {...question, options: [...question.options, newOption],
-    };
-}
+    /**
+     * Return a new version of the given question, with the `newOption` added to
+     * the list of existing `options`. Remember that the new Question MUST have
+     * its own separate copy of the `options` list, rather than the same reference
+     * to the original question's list!
+     * Check out the subsection about "Nested Fields" for more information.
+     */
+    export function addOption(question: Question, newOption: string): Question {
+        return { ...question, options: [...question.options, newOption] };
+    }
 
-/**
- * Consumes an id, name, and two questions, and produces a new question.
- * The new question will use the `body`, `type`, `options`, and `expected` of the
- * `contentQuestion`. The second question will provide the `points`.
- * The `published` status should be set to false.
- * Notice that the second Question is provided as just an object with a `points`
- * field; but the function call would be the same as if it were a `Question` type!
- */
-export function mergeQuestion(
-    id: number,
-    name: string,
-    contentQuestion: Question,
-    { points }: { points: number }
-): Question {
-    return {...contentQuestion, points, published: false,
-    };
+    /**
+     * Consumes an id, name, and two questions, and produces a new question.
+     * The new question will use the `body`, `type`, `options`, and `expected` of the
+     * `contentQuestion`. The second question will provide the `points`.
+     * The `published` status should be set to false.
+     * Notice that the second Question is provided as just an object with a `points`
+     * field; but the function call would be the same as if it were a `Question` type!
+     */
+    export function mergeQuestion(
+        id: number,
+        name: string,
+        contentQuestion: Question,
+        { points }: { points: number }
+    ): Question {
+        return { ...contentQuestion, points, published: false };
+    }
 }
