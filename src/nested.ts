ba770a6c1850814ece4899f5909a530833e3ae2e
mak <makpham@udel.edu>
2024-02-28 22:23:03 -0500

    Completed renameQuesitonById function

--- src/nested.ts
+++ src/nested.ts
@@ -178,7 +178,9 @@ export function renameQuestionById(
     targetId: number,
     newName: string
 ): Question[] {
-    return [];
+    return questions.map((question) =>
+        question.id === targetId ? { ...question, name: newName } : question
+    );
 }
 
 /***
